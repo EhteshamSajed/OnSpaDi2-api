18f5ffe3f1e010268a36d9d9d3369597264d3cf8
EhteshamSajed <ehteshamsajed@gmail.com>
2019-04-14 18:36:53 +0200

    Update CurrentParkingStateService.java

--- src/main/java/com/personal/OnSpaDi2/currentParkingState/CurrentParkingStateService.java
+++ src/main/java/com/personal/OnSpaDi2/currentParkingState/CurrentParkingStateService.java
@@ -1,10 +1,15 @@
 package com.personal.OnSpaDi2.currentParkingState;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.personal.OnSpaDi2.locationInformation.LocationInformation;
+import com.personal.OnSpaDi2.locationInformation.LocationInformationService;
 
 @Service
 public class CurrentParkingStateService {
@@ -12,15 +17,18 @@ public class CurrentParkingStateService {
 	@Autowired
 	private CurrentParkingStateRepository currentParkingStateRepository;
 	
+	@Autowired	
+	LocationInformationService locationInformationService = new LocationInformationService();
+	
 	public List<CurrentParkingState> getAllCurrentParkingStates() {
 		List<CurrentParkingState> currentParkingState = new ArrayList<>();
 		currentParkingStateRepository.findAll().forEach(currentParkingState::add);
 		return currentParkingState;
 	}
 	
-	public List<CurrentParkingState> getAllCurrentParkingStates(int id) {			//		wrong, make it for locationId
+	public List<CurrentParkingState> getAllCurrentParkingStatesForLocation(int locationId) {
 		List<CurrentParkingState> currentParkingState = new ArrayList<>();
-		//currentParkingStateRepository.findByParkingInformation_ParkingId(id).forEach(currentParkingState::add);
+		currentParkingStateRepository.findByLocationInformation_LocationId(locationId).forEach(currentParkingState::add);
 		return currentParkingState;
 	}
 	
@@ -46,4 +54,51 @@ public class CurrentParkingStateService {
 		currentParkingStateRepository.deleteById(id);
 		return new CurrentParkingStateResponse(null, "Deleted successfully");
 	}
+	
+	public List<Map<String, Object>> countParkingByLocation(String fields) {
+		List<LocationInformation> locationInformations = locationInformationService.getAllLocationInformation();		
+		int[] locationIds = new int[locationInformations.size()];		
+		List<Map<String, Object>> countMaps = new ArrayList<Map<String, Object>>();				
+		
+		for(int i=0; i<locationIds.length; i++) {
+			locationIds[i] = locationInformations.get(i).getLocationId();			
+			countMaps.add(countParkingByLocation(locationIds[i], fields));
+		}
+		
+		return countMaps;
+	}
+	
+	public Map<String, Object> countParkingByLocation(int locationId , String fields) {		
+		Map<String, Object> countMap = new HashMap<String, Object>();
+		
+		if (fields != null) {
+			String[] fieldsArray = fields.split(",");			
+
+			for (int i = 0; i < fieldsArray.length; i++) {
+				switch (fieldsArray[i]) {
+				case "total":
+					countMap.put("total", currentParkingStateRepository.countByLocationInformation_LocationId(locationId));
+					break;
+				case "free":
+					countMap.put("free", currentParkingStateRepository.countByParkingStateAndLocationInformation_LocationId("0", locationId));
+					break;
+				case "occupied":
+					countMap.put("occupied", currentParkingStateRepository.countByParkingStateAndLocationInformation_LocationId("1", locationId));
+					break;
+				case "locationId":
+					countMap.put("locationId", locationId);
+					break;
+				default:
+
+				}
+			}
+		} else {
+			countMap.put("total", currentParkingStateRepository.countByLocationInformation_LocationId(locationId));
+			countMap.put("free", currentParkingStateRepository.countByParkingStateAndLocationInformation_LocationId("0", locationId));
+			countMap.put("occupied", currentParkingStateRepository.countByParkingStateAndLocationInformation_LocationId("1", locationId));	
+			countMap.put("locationId", locationId);
+		}
+		
+		return countMap;
+	}	
 }
