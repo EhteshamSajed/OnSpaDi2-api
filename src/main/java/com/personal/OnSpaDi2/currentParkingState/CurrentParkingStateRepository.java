59ffa7fa5974448640989832b9fcc6bcb5b702d7
EhteshamSajed <ehteshamsajed@gmail.com>
2019-04-14 18:36:45 +0200

    Update CurrentParkingStateRepository.java

--- src/main/java/com/personal/OnSpaDi2/currentParkingState/CurrentParkingStateRepository.java
+++ src/main/java/com/personal/OnSpaDi2/currentParkingState/CurrentParkingStateRepository.java
@@ -2,9 +2,19 @@ package com.personal.OnSpaDi2.currentParkingState;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CurrentParkingStateRepository extends CrudRepository<CurrentParkingState, Long>{
 	public CurrentParkingState findByParkingInformation_ParkingId(Integer id);
+	public List<CurrentParkingState> findByLocationInformation_LocationId(Integer locationId);
+	public int countByLocationInformation_LocationId(int locationId);
+	public int countByParkingStateAndLocationInformation_LocationId(String parkingState, int locationId);
+	
+	/*@Query("SELECT DISTINCT location_id FROM current_parking_state")
+	public int[] findDistinctLocationId();*/
+	
+	//public List<CurrentParkingState> findDistinctLocationIdByParkingState
+	
 	//public CurrentParkingState findByParkingId(int id);
 }
