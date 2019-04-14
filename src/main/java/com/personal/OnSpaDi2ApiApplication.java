9cc7f8054835db9210f9723788bf266ff0e40b5e
EhteshamSajed <ehteshamsajed@gmail.com>
2019-04-14 18:37:41 +0200

    Update OnSpaDi2ApiApplication.java

--- src/main/java/com/personal/OnSpaDi2ApiApplication.java
+++ src/main/java/com/personal/OnSpaDi2ApiApplication.java
@@ -2,11 +2,11 @@ package com.personal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class OnSpaDi2ApiApplication {
-
-	//@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		SpringApplication.run(OnSpaDi2ApiApplication.class, args);
 	}
