0ba3d0924b0b82955929d2116f86cf6a3f37c7b0
EhteshamSajed <ehteshamsajed@gmail.com>
2019-04-14 18:36:49 +0200

    Update CurrentParkingStateController.java

--- src/main/java/com/personal/OnSpaDi2/currentParkingState/CurrentParkingStateController.java
+++ src/main/java/com/personal/OnSpaDi2/currentParkingState/CurrentParkingStateController.java
@@ -1,17 +1,19 @@
 package com.personal.OnSpaDi2.currentParkingState;
 
 import java.util.List;
-
-import javax.websocket.server.PathParam;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.personal.OnSpaDi2.locationInformation.LocationInformation;
 import com.personal.OnSpaDi2.parkingInformation.ParkingInformation;
+import com.personal.OnSpaDi2.parkingInformation.ParkingInformationService;
 
 @RestController
 public class CurrentParkingStateController {
@@ -19,47 +21,58 @@ public class CurrentParkingStateController {
 	@Autowired
 	private CurrentParkingStateService currentParkingStateService;
 	
+	@Autowired
+	private ParkingInformationService parkingInformationService;
+	
 	@RequestMapping("/currentParkingState")
 	public List<CurrentParkingState> getAllCurrentParkingState(){
 		return currentParkingStateService.getAllCurrentParkingStates();
 	}
 	
 	@RequestMapping("/parkingInformation/{id}/currentParkingState")
-	public List<CurrentParkingState> getAllCurrentParkingState(@PathVariable int id){				//		wrong, make it for locationId
-		return currentParkingStateService.getAllCurrentParkingStates(id);
-	}	
+	public CurrentParkingState getAllCurrentParkingState(@PathVariable int id){
+		return currentParkingStateService.getcurrentParkingStateByParkingId(id);
+	}
 	
 	@RequestMapping("/currentParkingState/{id}")
 	public CurrentParkingState getCurrentParkingStateById(@PathVariable long id) {
 		return currentParkingStateService.getcurrentParkingStateById(id);
-	}
-	
-	@RequestMapping("/currentParkingStateForParkingId/{id}")										//		remove it
-	public CurrentParkingState getCurrentParkingStateByParkingId(@PathVariable int id) {
-		return currentParkingStateService.getcurrentParkingStateByParkingId(id);
-	}
+	}	
 	
 	@RequestMapping("/locationInformation/{locationId}/currentParkingState")
-	public List<CurrentParkingState> getAllCurrentParkingStatesByLocation(@PathVariable int locationId){				//		wrong, make it for locationId
-		return currentParkingStateService.getAllCurrentParkingStates(locationId);
+	public List<CurrentParkingState> getAllCurrentParkingStatesByLocation(@PathVariable int locationId){
+		return currentParkingStateService.getAllCurrentParkingStatesForLocation(locationId);
 	}
 	
-	@RequestMapping(method = RequestMethod.POST, value = "/currentParkingState")
-	public CurrentParkingStateResponse addCurrentParkingState(@RequestBody CurrentParkingState currentParkingState) {
-		return currentParkingStateService.addCurrentParkingState(currentParkingState);		
+	@RequestMapping("/countParkings")
+	public List<Map<String, Object>> countParkings(@RequestParam(required = false) String fields){		
+		return currentParkingStateService.countParkingByLocation(fields);
 	}
+	
+	@RequestMapping("/locationInformation/{locationId}/countParkings")
+	public Map<String, Object> countParkingsByLocation(@PathVariable int locationId , @RequestParam(required = false) String fields){		
+		return currentParkingStateService.countParkingByLocation(locationId, fields);
+	}
+	
+	/*@RequestMapping(method = RequestMethod.POST, value = "/currentParkingState")
+	public CurrentParkingStateResponse addCurrentParkingState(@RequestBody CurrentParkingState currentParkingState) {
+		System.out.println(currentParkingState);
+		return currentParkingStateService.addCurrentParkingState(currentParkingState);		
+	}*/
 		
-	@RequestMapping(method = RequestMethod.POST, value = "/parkingInformation/{parkingInformationId}/currentParkingState")
-	public CurrentParkingStateResponse addCurrentParkingState(@RequestBody CurrentParkingState _currentParkingState, @PathVariable int parkingInformationId) {
+	@RequestMapping(method = RequestMethod.POST, value = "/parkingInformation/{parkingId}/currentParkingState")
+	public CurrentParkingStateResponse addCurrentParkingState(@RequestBody CurrentParkingState _currentParkingState, @PathVariable int parkingId) {
 		
-		CurrentParkingState currentParkingState =  currentParkingStateService.getcurrentParkingStateByParkingId(parkingInformationId);
+		CurrentParkingState currentParkingState =  currentParkingStateService.getcurrentParkingStateByParkingId(parkingId);
 		if(currentParkingState != null)					//		if exists
 		{			
 			currentParkingState.setSensorState(_currentParkingState.getSensorState());
 			currentParkingState.setParkingState(_currentParkingState.getParkingState());
 		}else {											//		new entry			
 			currentParkingState = _currentParkingState;
-			currentParkingState.setParkingInformation(new ParkingInformation(parkingInformationId, 0, 0));
+			currentParkingState.setParkingInformation(new ParkingInformation(parkingId, 0, 0));
+			int locationId = parkingInformationService.getParkingInformationById(parkingId).getLocationInformation().getLocationId();
+			currentParkingState.setLocationInformation(new LocationInformation(locationId));
 		}
 		
 		
