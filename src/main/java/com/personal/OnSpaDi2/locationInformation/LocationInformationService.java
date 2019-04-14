76a7ac0ffacc4f73222cbac1e023a41fb2580bb6
EhteshamSajed <ehteshamsajed@gmail.com>
2019-04-14 18:37:05 +0200

    Update LocationInformationService.java

--- src/main/java/com/personal/OnSpaDi2/locationInformation/LocationInformationService.java
+++ src/main/java/com/personal/OnSpaDi2/locationInformation/LocationInformationService.java
@@ -1,10 +1,19 @@
 package com.personal.OnSpaDi2.locationInformation;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.personal.OnSpaDi2.parkingInformation.ParkingInformation;
 
 @Service
 public class LocationInformationService {
@@ -41,5 +50,53 @@ public class LocationInformationService {
 	public LocationInformationResponse deleteLocationInformation(int id) {
 		locationInformationRepository.deleteById(id);
 		return new LocationInformationResponse(null, "Deleted successfully");
+	}
+	
+	public Map<String, Object> convertLocationInformationToMap(LocationInformation locationInformation, String fields){
+		
+		Map<String, Object> informationMap = new HashMap<String, Object>();
+
+		if (fields != null) {
+			String[] fieldsArray = fields.split(",");
+
+			for (int i = 0; i < fieldsArray.length; i++) {
+				switch (fieldsArray[i]) {
+				case "locationId":
+					informationMap.put(fieldsArray[i], locationInformation.getLocationId());
+					break;
+				case "locationName":
+					informationMap.put(fieldsArray[i], locationInformation.getLocationName());
+					break;
+				case "latitude":
+					informationMap.put(fieldsArray[i], locationInformation.getLatitude());
+					break;
+				case "longitude":
+					informationMap.put(fieldsArray[i], locationInformation.getLongitude());
+					break;
+				case "totalParkings":
+					informationMap.put(fieldsArray[i], locationInformation.getTotalParkings());
+					break;
+				case "createdOn":
+					informationMap.put(fieldsArray[i], locationInformation.getCreatedOn());
+					break;
+				case "parkingInformations":
+					informationMap.put(fieldsArray[i], locationInformation.getParkingInformations());
+					break;
+
+				default:
+
+				}
+			}
+		}else {
+			informationMap.put("locationId", locationInformation.getLocationId());
+			informationMap.put( "locationName", locationInformation.getLocationName());
+			informationMap.put("latitude", locationInformation.getLatitude());
+			informationMap.put("longitude", locationInformation.getLongitude());
+			informationMap.put("totalParkings", locationInformation.getTotalParkings());
+			informationMap.put("createdOn", locationInformation.getCreatedOn());
+			informationMap.put("parkingInformations", locationInformation.getParkingInformations());
+		}		
+		
+		return informationMap;
 	}
 }
